41351b0b2b03c420472eacbcd64c79fb5be8eebd
AHosting <ahosting2023@gmail.com>
2025-07-28 23:09:51 +0530

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Downloading Stockfish..."'
+echo "Downloading Stockfish....."'
 curl -L -o stockfish.tar "https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64-avx2.tar"
 tar -xf stockfish.tar
 mv stockfish* stockfish
